306aabe3de348f58e6ce1a3b8885a897029a680b
Vanadse <legend_of_mana1@yahoo.com.ar>
2025-06-04 10:56:21 -0300

    added Leaf Nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,10 +13,33 @@ class HTMLNode():
     
     def props_to_html(self):
         result = ""
+        if self.props == None:
+            return None
         for prop in self.props:
             result = result + f' {prop}="{self.props[prop]}"'
         return result
     
     def __repr__(self):
-        return "HTMLNode(" + f"{self.tag}, " + f"{self.value}" + f", {self.children}" + f",{self.props_to_html()})"
+        return "HTMLNode(" + f"<{self.tag}>, " + f"{self.value}" + f", {self.children}" + f",{self.props_to_html()})"
 
+class LeafNode(HTMLNode):
+
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None or self.value == "":
+            raise ValueError
+        
+        if self.tag == None:
+            return self.value
+        
+        if self.tag == "p" or self.tag == "b" or self.tag == "i":
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+    
+        if self.tag == "a":
+            return f"<{self.tag}{self.props_to_html()}<{self.tag}>"
+        
+    def __repr__(self):
+        return "LeafNode(" + f"{self.to_html()})"
+        #return "LeafNode(" + f"{self.tag}, " + f"{self.value}" + f",{self.props_to_html()})"
--- src/main.py
+++ src/main.py
@@ -13,4 +13,11 @@ def main():
     test_HTML_node = HTMLNode("p", "This is a test paragraph", None, test_dict)
     print (test_HTML_node)
 
+    test_Leaf_node = LeafNode("p", "This is a paragraph of text.")
+    print (test_Leaf_node)
+
+    test_Leaf_node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+    print (test_Leaf_node)
+
+
 main()
